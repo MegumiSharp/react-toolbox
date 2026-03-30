337e994789eb5aa32ac2ce607f4851aa95aa0ea6
Simone <simone.enselmi@gmail.com>
2026-03-30 16:18:21 +0200

    feat: Add NotFoundPage - NotFoundPage is showed everytime the router cant find the url path - NotFoundPage have the frontend implemented with a button to return home

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,12 +7,14 @@ import './index.css'
 import App from './App.tsx'
 import ColorPickerPage from './pages/ColorPickerPage/ColorPickerPage';
 import ToDoListPage from './pages/ToDoListPage/ToDoListPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage.tsx';
 
 
 const router = createBrowserRouter([
     {path: "/", element: <App/>},
     {path: "/colorpicker", element: <ColorPickerPage/>},
-    {path: "/todolist", element: <ToDoListPage/>}
+    {path: "/todolist", element: <ToDoListPage/>},
+    {path: "*", element: <NotFoundPage/>}
 ])
 
 createRoot(document.getElementById('root')!).render(
